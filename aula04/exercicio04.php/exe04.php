6e136bab074254e50b592403b920f5fa7bc73965
isaribeirooo <161508466+isaribeirooo@users.noreply.github.com>
2025-03-07 18:34:33 -0300

    Add files via upload

---
+++ aula04/exercicio04.php/exe04.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 04</title>
+</head>
+<body>
+    <h1>Formulario</h1>
+    <form action="processa04.php" method="post">
+    <p>Digite seu nome: <input type="text" name="cxnome" id=""></p>
+    <p>Digite seu email: <input type="text" name="cxemail" id=""></p>
+    <p>Digite seu email: <input type="date" name="cxdata" id=""></p>
+    <select name="cartões" id="cartões">
+  <option value="visa">visa</option>
+  <option value="mastercard">mastercard</option>
+  <option value="elo">elo</option>
+  <option value="american express">american express</option>
+</select>
+
+
+
+    <input type="submit" value="ENVIAR">
+
+    </form>
+</body>
+</html>
---
+++ aula04/exercicio04.php/processa04.php
@@ -0,0 +1,15 @@
+<?php
+   $nome = $_POST['cxnome'];
+   $email = $_POST['cxemail'];
+   $data = $_POST['cxdata'];
+
+   
+
+   echo "Aqui estão seus dados: ,$nome";
+   echo "$email";
+   echo "$data";
+
+
+
+
+?>

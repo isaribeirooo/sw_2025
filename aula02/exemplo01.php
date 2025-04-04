1450777d408d48c12d0b012f79669e820336af2a
isaribeirooo <isadoraribeiro2708@gmail.com>
2025-04-04 15:43:17 -0300

    ajustes nas primeiras aulas

---
+++ aula02/exemplo01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+
+<h1>ESSE É O SITE DA ETEC - ISADORA RIBEIRO</h1>
+    <p>Oi sou a Isadora</p>
+ <?php
+ echo "<p>Oi sou a Isadora</p>";
+ $nome= "Isadora";
+ echo "<p> Oi sou a $nome</p>";
+ echo "<p> Oi sou a " .$nome." </p>";
+?>
+
+</body>
+</html>
--- processa02.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $nome = $_POST['cxnome'];
-   echo "Bem vindo, $nome";
-   echo"<br>";
-   $profissão = $_POST['cxprofissão'];
-   echo "Vc é formado(a) em: $profissão";
-
-?>
